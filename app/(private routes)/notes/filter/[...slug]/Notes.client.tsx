6977e390c91ce62b45778e93ef4b9f0b7060ab03
KateB713 <kbairachnaya@gmail.com>
2026-06-17 17:54:17 +0300

    fix structure

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -6,10 +6,10 @@ import { useDebouncedCallback } from 'use-debounce';
 import Link from 'next/link';
 import css from './Notes.module.css';
 
-import { fetchNotes } from '../../../../lib/api';
-import NoteList from '../../../../components/NoteList/NoteList';
-import Pagination from '../../../../components/Pagination/Pagination';
-import SearchBox from '../../../../components/SearchBox/SearchBox';
+import { fetchNotes } from '../../../../../lib/api';
+import NoteList from '../../../../../components/NoteList/NoteList';
+import Pagination from '../../../../../components/Pagination/Pagination';
+import SearchBox from '../../../../../components/SearchBox/SearchBox';
 
 interface NotesClientProps {
   tag?: string;
